35e6fde1dc1a6837665b3c0964dc2337344dad59
Alex Ares Marin <alex.ares.marin@gmail.com>
2025-06-01 14:33:26 +0200

    Integrate Clerk for authentication and set up initial app structure
    - Added `@clerk/clerk-expo` for user authentication in the app.
    - Created a new `InitialLayout` component to manage authentication flow.
    - Implemented login screen with Google SSO functionality.
    - Set up tab navigation with screens for home, bookmarks, create, notifications, and profile.
    - Updated styles and theme constants for consistent design.
    - Added an illustration for the login screen.
    - Configured the app layout to handle user sessions and redirects based on authentication state.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,28 @@
-import { Stack } from "expo-router";
+import InitialLayout from "@/components/InitialLayout";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
+            <StatusBar style="light" />
+            <InitialLayout />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
+  );
 }
