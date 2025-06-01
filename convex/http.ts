4f8295f2b7926a91c8bec67da6a2506fb21acb66
Alex Ares Marin <alex.ares.marin@gmail.com>
2025-06-01 17:59:01 +0200

    Enhance post creation functionality with image upload support
    - Added `expo-image-picker` and `expo-file-system` dependencies for image selection and upload capabilities.
    - Implemented a new `CreateScreen` component for creating posts, allowing users to select images and add captions.
    - Introduced `generateUploadUrl` and `createPost` mutations in the Convex API for handling image uploads and post creation.
    - Created a new `create.styles.ts` file for styling the post creation interface.
    - Updated `package.json` and `package-lock.json` to include new dependencies.

--- convex/http.ts
+++ convex/http.ts
@@ -5,6 +5,10 @@ import { httpAction } from "./_generated/server";
 
 const http = httpRouter();
 
+// 1- we need to make sure that the webhook event is coming from clerk
+// 2- if so, we will listen for the "user.created" event
+// 3- we will save the user to the database
+
 http.route({
   path: "/clerk-webhooks",
   method: "POST",
